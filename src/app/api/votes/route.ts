d89c08a06107e8761c6d9e78bc5332afc907b637
holoscopecheck <holoscopecheck@gmail.com>
2025-11-04 10:19:27 +0900

    implement api to post poll info

---
+++ src/app/api/votes/route.ts
@@ -0,0 +1,75 @@
+import { prisma } from "@/app/lib/prisma";
+import { parse } from "cookie";
+import { NextRequest, NextResponse } from "next/server";
+import { verifyToken } from "@/app/lib/auth/jwt";
+
+type Project = {
+  teamName: string;
+  projectName: string;
+  description?: string;
+  vote: number;
+};
+
+export async function POST(req: NextRequest) {
+  try {
+    const cookieHeader = req.headers.get("cookie");
+    if (!cookieHeader) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const cookies = parse(cookieHeader);
+    const token = cookies.token;
+
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = await req.json();
+    const { title, projects } = body;
+
+    if (!title || !projects || !Array.isArray(projects)) {
+      return NextResponse.json(
+        { message: "Title and projects are required" },
+        { status: 400 }
+      );
+    }
+
+    const secret = process.env.SECRET_KEY;
+    if (!secret) {
+      console.error("SECRET_KEY is not set");
+      return NextResponse.json(
+        { message: "Internal server error" },
+        { status: 500 }
+      );
+    }
+
+    const decoded = verifyToken(token, secret) as { userid: number };
+
+    // 1つのPollと複数のProjectを同時に作成
+    const newPoll = await prisma.poll.create({
+      data: {
+        title,
+        createdBy: decoded.userid,
+        projects: {
+          create: projects.map((project: Project) => ({
+            teamName: project.teamName,
+            projectName: project.projectName,
+            description: project.description || null,
+            votes: project.vote,
+          })),
+        },
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Vote created successfully", voteId: newPoll.id },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
